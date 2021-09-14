ec71b0b33cb6bd601f93f02625847700a384725e
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-14 10:26:07 +0200

    add initial logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import CalcButton from './CalcButton';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      calcObj: {},
+    };
+    this.handleCalculate = this.handleCalculate.bind(this);
+  }
+
+  handleCalculate(calcObj) {
+    this.setState({ calcObj: calculate(calcObj) });
   }
 
   render() {
+    const { calcObj } = this.state;
+
+    let screen = '0';
+    if (Object.keys(calcObj).length === 0 || (calcObj.total && calcObj.next && calcObj.operator === null)) {
+      screen = '0';
+    } else {
+      screen = calcObj.next != null ? calcObj.next : calcObj.total;
+    }
+
     return (
       <div className="calc-body">
         <div className="calc-screen">
-          0
+          {screen}
         </div>
         <div className="keyboard">
           <div className="key-digits">
